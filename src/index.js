5c983c029d85b2a0d7f5f45b643c4708ee1164b2
ajkelso <ajkelso@gmail.com>
2021-01-03 14:40:52 -0500

    commit tree 7d99e4e53b2edb5c151f65182e26088b85adf60e

--- src/index.js
+++ src/index.js
@@ -13,3 +13,42 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+const toyCollection = document.getElementById('toy-collection')
+fetch('http://localhost:3000/toys')
+ .then( res => res.json() )
+ .then( function(obj) {
+    obj.forEach(function(toy){ 
+      createCard(toy)
+    })
+  })
+
+function createCard(toy) {
+  const div = document.createElement('div')
+  div.classList.add("card")
+  const info = []
+  //h2
+  const h2 = document.createElement('h2')
+  h2.innerHTML = toy.name
+  //img
+  const img = document.createElement('img')
+  img.src = toy.image
+  img.classList.add("toy-avatar")
+  //p
+  const p = document.createElement('p')
+  p.innerHTML = `${toy.likes} Likes`
+  //button
+  const button = document.createElement('button')
+  button.innerHTML = "Like"
+  button.classList.add("like-btn")
+  
+  info.push(h2, img, p, button)
+
+  appendChildren(div, info)
+  toyCollection.appendChild(div)
+}
+
+function appendChildren(parent, children) {
+  children.forEach(function (child) {
+    parent.appendChild(child);
+  })
+}
